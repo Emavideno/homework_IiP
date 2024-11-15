d32769bfcd3a222e35cfc4a14da41b4182dad20f
Emavideno <166414249+Emavideno@users.noreply.github.com>
2024-11-15 23:26:20 +0300

    Add files via upload

---
+++ Homework_15_11_ULTIMATE/3_massives/Task3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_massives
+{
+    internal class Task3Class
+    {
+        public static void Task3() 
+        {
+            int[,,] cube = new int[6, 3, 3]
+            {
+                {
+                    { 0, 0, 0 },
+                    { 0, 1, 0 },
+                    { 0, 0, 0 }
+                },
+                {
+                    { 0, 0, 0 },
+                    { 1, 0, 1 },
+                    { 0, 0, 0 }
+                },
+                {
+                    { 0, 0, 0 },
+                    { 1, 1, 1 },
+                    { 0, 0, 0 }
+                },
+                {
+                    { 1, 0, 1 },
+                    { 0, 0, 0 },
+                    { 1, 0, 1 }
+                },
+                {
+                    { 1, 0, 1 },
+                    { 0, 1, 0 },
+                    { 1, 0, 1 }
+                },
+                {
+                    { 1, 0, 1 },
+                    { 1, 0, 1 },
+                    { 1, 0, 1 }
+                }
+            };
+
+            Random random = new Random();
+            int faceIndex = random.Next(6);
+
+            Console.WriteLine($"Результат броска (Грань {faceIndex + 1})");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(cube[faceIndex, i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
